2a67802f5e26fe37b168ff0d5b4bdb9e63ed7f93
JKToo <49129827+JKToo@users.noreply.github.com>
2021-05-08 19:58:36 -0400

    Update Pennies for pay.java

--- Pennies for pay.java
+++ Pennies for pay.java
@@ -1,8 +1,8 @@
-Write a program that calculates the amount a person would earn over a period of time if his or her salary is one penny the first day,
+/*Write a program that calculates the amount a person would earn over a period of time if his or her salary is one penny the first day,
 two pennies the second day, and continues to double each day. Theprogram should display a table showing 
 the salary for each day, and then show the total pay at the end of the period. The output should be displayed in a dollar Problem amount, 
 not the number of pennies.
-Input Validation: Do not accept a numher less than t for the number of days worked
+Input Validation: Do not accept a numher less than t for the number of days worked*/
 
 _____________________________________________________________________________________________________________________________
 import java.util.Scanner;
