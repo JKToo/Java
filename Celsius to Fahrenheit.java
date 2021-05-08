fb1bdf72f519dc1017e13c12e95b562cadeb7d71
JKToo <49129827+JKToo@users.noreply.github.com>
2021-05-08 19:59:16 -0400

    Update Celsius to Fahrenheit.java

--- Celsius to Fahrenheit.java
+++ Celsius to Fahrenheit.java
@@ -1,7 +1,7 @@
-Write a program that displays a table of the Celsius temperatures 0 through 20 and their Fahrenheit equivalents. 
+/*Write a program that displays a table of the Celsius temperatures 0 through 20 and their Fahrenheit equivalents. 
 The formula for converting a temperature from Celsius to Fahrenheit is
 F = 9/5 C + 32 where F is the Fahrenheit temperature and C is the Celsius temperature. Your program must use a loop to display the table.
-______________________________________________________________________________________________________________________________________
+______________________________________________________________________________________________________________________________________*/
 public class Celcuis
 {
 	public static void main (String [] args)
